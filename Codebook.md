f93fcaf9689d238a2e4d0e805f2336055d9d049e
ivan2016ds <ivantioh@outlook.com>
2016-08-14 21:28:44 +0800

    Added run_analysis.R and Codebook.md

---
+++ Codebook.md
@@ -0,0 +1,51 @@
+### Description
+
+The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz. 
+
+Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag). 
+
+Finally a Fast Fourier Transform (FFT) was applied to some of these signals producing fBodyAcc-XYZ, fBodyAccJerk-XYZ, fBodyGyro-XYZ, fBodyAccJerkMag, fBodyGyroMag, fBodyGyroJerkMag. (Note the 'f' to indicate frequency domain signals). 
+
+### Signals (Measurements)
+
+These signals were used to estimate variables of the feature vector for each pattern:  
+'-XYZ' is used to denote 3-axial signals in the X, Y and Z directions.
+
+tBodyAcc-XYZ
+tGravityAcc-XYZ
+tBodyAccJerk-XYZ
+tBodyGyro-XYZ
+tBodyGyroJerk-XYZ
+tBodyAccMag
+tGravityAccMag
+tBodyAccJerkMag
+tBodyGyroMag
+tBodyGyroJerkMag
+fBodyAcc-XYZ
+fBodyAccJerk-XYZ
+fBodyGyro-XYZ
+fBodyAccMag
+fBodyAccJerkMag
+fBodyGyroMag
+fBodyGyroJerkMag
+
+### Metrics
+
+Metrics used for these signals are: 
+
+mean(): Mean value
+std(): Standard deviation
+
+### Dataset Information
+
+Observations: 181
+Variables: 81
+...Average of measurements: 79
+......Type: Numeric value
+......Average of mean/ standard deviation values for each signals (measurements) 
+...Subject: 1
+......Type: Factor
+......Subject/ respondent
+...Activity: 1
+......Type: Factor
+......Activity being carried out
---
+++ run_analysis.R
@@ -0,0 +1,57 @@
+## set working directory
+##setwd("c3w4-project") ## NOT RUN
+##list.files()          ## NOT RUN
+
+## getting data
+xTrain <- read.table("UCI HAR Dataset/train/X_train.txt")
+yTrain <- read.table("UCI HAR Dataset/train/y_train.txt")
+subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+xTest <- read.table("UCI HAR Dataset/test/X_test.txt")
+yTest <- read.table("UCI HAR Dataset/test/y_test.txt")
+subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
+
+## merge to single dataset
+## train set
+names(xTrain) <- features[,2]           # set variable names
+names(subjectTrain) <- "subject"        # set variable names
+names(yTrain) <- "activity"             # set variable names
+mergedTrain <- cbind(xTrain,subjectTrain,yTrain) 
+names(mergedTrain)
+
+## test set
+names(xTest) <- features[,2]            # set variable names
+names(subjectTest) <- "subject"         # set variable names
+names(yTest) <- "activity"              # set variable names
+mergedTest <- cbind(xTest,subjectTest,yTest) 
+names(mergedTest)
+
+## final data set
+finalData <- rbind(mergedTrain,mergedTest)
+
+## extract only mean and std for each measurement variables
+meanElem <- names(finalData)[grep("mean()",names(finalData))]
+stdElem <- names(finalData)[grep("std()",names(finalData))]
+meanstdSub <- c(meanElem,stdElem,"subject","activity")
+finalData <- finalData[,meanstdSub]
+
+## function to replace activity name
+updateActivityNames <- function(x){
+        return(activityLabels[x,"V2"])
+}
+
+## naming activity descriptively
+length(which(is.na(finalData$activity)))        # check if activity contain NA
+finalData$activity <- sapply(finalData$activity,updateActivityNames)
+length(which(is.na(finalData$activity)))        # check again if activity contain NA
+
+## data set with average of variables
+library(dplyr)
+groupedData <- group_by(finalData,activity,subject)
+summaryData <- summarize_each(groupedData,funs(mean))
+
+## writing to txt
+write.table(summaryData,"tidyData.txt",row.names=FALSE)
